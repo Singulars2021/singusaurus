82fc0658bc65ed13b872ea5818219f8123ba33ab
Jesús C. Moreno <jesuscm@MacBook-Air-de-Jesus.local>
2021-01-11 14:21:57 +0100

    UPDATE: Deleted unused Feature function

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
